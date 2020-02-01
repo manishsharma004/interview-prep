3eb4477d01e6ef70754d32a09409e15493e4500e
mannyhappenings <manishsharma004@yahoo.com>
2020-02-01 10:57:10 +0530

    updated scripts

--- hackerrank/scripts/test_java.sh
+++ hackerrank/scripts/test_java.sh
@@ -6,14 +6,15 @@ function compile_and_test() {
     then
 	program_name=`basename "$PWD"`
     fi
+    program_name=`echo "$program_name" | awk -F"-" '{for(i=1;i<=NF;i++){$i=toupper(substr($i,1,1)) substr($i,2)}} 1' OFS=""`
     if [ ! -d build ]; then
         mkdir -m 775 build
     fi
 
-    g++ ${program_name}.cpp -o build/${program_name} \
+    javac ${program_name}.java -d build/ \
         && ls -1 inputs | while read input_file_name; do
             echo "-- Testing with input from '${input_file_name}':";
-            build/${program_name} < inputs/${input_file_name};
+            java -classpath build/ ${program_name} < inputs/${input_file_name};
         done
 }
 
